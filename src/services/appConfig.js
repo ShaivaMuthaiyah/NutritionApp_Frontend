9ead6bde50d4706957727a84f9ba089103cd4433
unknown <shaivamuthaiya@gmail.com>
2025-01-05 12:25:06 +0530

    troubleshooting

--- src/services/appConfig.js
+++ src/services/appConfig.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || window._env_.REACT_APP_API_URL || "http://localhost/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost/api";
 const BUCKET_URL = proces.env.BUCKET_URL
 
 export { API_URL, BUCKET_URL };
