f96e343b61517512929c80f9bedb64524228adc5
unknown <shaivamuthaiya@gmail.com>
2025-01-05 12:55:00 +0530

    variable issue

--- src/services/appConfig.js
+++ src/services/appConfig.js
@@ -1,4 +1,5 @@
-const API_URL = process.env.REACT_APP_API_URL
-const BUCKET_URL = proces.env.BUCKET_URL
+export const API_URL = process.env.REACT_APP_API_URL;
+export const BUCKET_URL = process.env.BUCKET_URL;
 
-export default { API_URL, BUCKET_URL };
+
+// export default { API_URL, BUCKET_URL };
