7f63ea036a63bceaad0b20dd88a24d6e6aede7c0
unknown <shaivamuthaiya@gmail.com>
2025-02-07 12:03:30 +0530

    small change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,7 +63,7 @@ export default function Navbar() {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <p className='hidden sm:block text-gray-300'>Welcome to Amritha !</p>
+            <p className='hidden sm:block text-gray-300'>Welcome to CookSmart!</p>
             {/* <button
               type="button"
               className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
